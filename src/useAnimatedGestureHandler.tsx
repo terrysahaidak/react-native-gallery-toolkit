95831843f61f50ad23c895eed6b19eab76a07622
Terry Sahaidak <s_tasssik_taras@ukr.net>
2020-08-23 03:13:13 +0300

    extract handler creator out of useAnimatedGestureEvent

--- src/useAnimatedGestureHandler.tsx
+++ src/useAnimatedGestureHandler.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useCallback } from 'react';
 import { Platform } from 'react-native';
 import Animated, {
   makeRemote,
@@ -92,119 +92,133 @@ interface GestureHandlers<T, TContext extends Context> {
 
 type OnGestureEvent<T> = (event: T) => void;
 
+export function createAnimatedGestureHandler<
+  T extends GestureHandlerGestureEvent,
+  TContext extends Context
+>(handlers: GestureHandlers<T['nativeEvent'], TContext>) {
+  const context = useRemoteContext<any>({
+    __initialized: false,
+  });
+  const isAndroid = Platform.OS === 'android';
+
+  const handler = (
+    event: T['nativeEvent'] &
+      GestureHandlerStateChangeEvent['nativeEvent'],
+  ) => {
+    'worklet';
+
+    // const UNDETERMINED = 0;
+    const FAILED = 1;
+    const BEGAN = 2;
+    const CANCELLED = 3;
+    const ACTIVE = 4;
+    const END = 5;
+
+    if (handlers.onInit && !context.__initialized) {
+      context.__initialized = true;
+      handlers.onInit(event, context);
+    }
+
+    if (handlers.onGesture) {
+      handlers.onGesture(event, context);
+    }
+
+    const stateDiff = diff(context, 'pinchState', event.state);
+
+    const pinchBeganAndroid =
+      stateDiff === ACTIVE - BEGAN ? event.state === ACTIVE : false;
+
+    const isBegan = isAndroid
+      ? pinchBeganAndroid
+      : event.state === BEGAN;
+
+    if (isBegan) {
+      if (handlers.shouldHandleEvent) {
+        context._shouldSkip = !handlers.shouldHandleEvent(
+          event,
+          context,
+        );
+      } else {
+        context._shouldSkip = false;
+      }
+    } else if (typeof context._shouldSkip === 'undefined') {
+      return;
+    }
+
+    if (!context._shouldSkip) {
+      if (handlers.onEvent) {
+        handlers.onEvent(event, context);
+      }
+
+      if (handlers.beforeEach) {
+        handlers.beforeEach(event, context);
+      }
+
+      if (isBegan && handlers.onStart) {
+        handlers.onStart(event, context);
+      }
+
+      if (event.state === ACTIVE && handlers.onActive) {
+        handlers.onActive(event, context);
+      }
+      if (
+        event.oldState === ACTIVE &&
+        event.state === END &&
+        handlers.onEnd
+      ) {
+        handlers.onEnd(event, context);
+      }
+      if (
+        event.oldState === ACTIVE &&
+        event.state === FAILED &&
+        handlers.onFail
+      ) {
+        handlers.onFail(event, context);
+      }
+      if (
+        event.oldState === ACTIVE &&
+        event.state === CANCELLED &&
+        handlers.onCancel
+      ) {
+        handlers.onCancel(event, context);
+      }
+
+      if (event.oldState === ACTIVE) {
+        if (handlers.onFinish) {
+          handlers.onFinish(
+            event,
+            context,
+            event.state === CANCELLED || event.state === FAILED,
+          );
+        }
+      }
+
+      if (handlers.afterEach) {
+        handlers.afterEach(event, context);
+      }
+    }
+
+    if (event.oldState === ACTIVE) {
+      context._shouldSkip = undefined;
+    }
+  };
+
+  return handler;
+}
+
 export function useAnimatedGestureHandler<
   T extends GestureHandlerGestureEvent,
   TContext extends Context
 >(
   handlers: GestureHandlers<T['nativeEvent'], TContext>,
 ): OnGestureEvent<T> {
-  const context = useRemoteContext<any>({
-    __initialized: false,
-  });
-  const isAndroid = Platform.OS === 'android';
-
-  return useEvent(
-    (
-      event: T['nativeEvent'] &
-        GestureHandlerStateChangeEvent['nativeEvent'],
-    ) => {
-      'worklet';
-
-      // const UNDETERMINED = 0;
-      const FAILED = 1;
-      const BEGAN = 2;
-      const CANCELLED = 3;
-      const ACTIVE = 4;
-      const END = 5;
-
-      if (handlers.onInit && !context.__initialized) {
-        context.__initialized = true;
-        handlers.onInit(event, context);
-      }
-
-      if (handlers.onGesture) {
-        handlers.onGesture(event, context);
-      }
-
-      const stateDiff = diff(context, 'pinchState', event.state);
-
-      const pinchBeganAndroid =
-        stateDiff === ACTIVE - BEGAN ? event.state === ACTIVE : false;
-
-      const isBegan = isAndroid
-        ? pinchBeganAndroid
-        : event.state === BEGAN;
-
-      if (isBegan) {
-        if (handlers.shouldHandleEvent) {
-          context._shouldSkip = !handlers.shouldHandleEvent(
-            event,
-            context,
-          );
-        } else {
-          context._shouldSkip = false;
-        }
-      } else if (typeof context._shouldSkip === 'undefined') {
-        return;
-      }
-
-      if (!context._shouldSkip) {
-        if (handlers.onEvent) {
-          handlers.onEvent(event, context);
-        }
-
-        if (handlers.beforeEach) {
-          handlers.beforeEach(event, context);
-        }
-
-        if (isBegan && handlers.onStart) {
-          handlers.onStart(event, context);
-        }
-
-        if (event.state === ACTIVE && handlers.onActive) {
-          handlers.onActive(event, context);
-        }
-        if (
-          event.oldState === ACTIVE &&
-          event.state === END &&
-          handlers.onEnd
-        ) {
-          handlers.onEnd(event, context);
-        }
-        if (
-          event.oldState === ACTIVE &&
-          event.state === FAILED &&
-          handlers.onFail
-        ) {
-          handlers.onFail(event, context);
-        }
-        if (
-          event.oldState === ACTIVE &&
-          event.state === CANCELLED &&
-          handlers.onCancel
-        ) {
-          handlers.onCancel(event, context);
-        }
-
-        if (event.oldState === ACTIVE) {
-          if (handlers.onFinish) {
-            handlers.onFinish(
-              event,
-              context,
-              event.state === CANCELLED || event.state === FAILED,
-            );
-          }
-        }
-
-        if (handlers.afterEach) {
-          handlers.afterEach(event, context);
-        }
-      }
-
-      if (event.oldState === ACTIVE) {
-        context._shouldSkip = undefined;
-      }
-    },
-    ['onGestureHandlerStateChange', 'onGestureHandlerEvent'],
+  const handler = useCallback(
+    createAnimatedGestureHandler<T, TContext>(handlers),
+    [],
   );
+
+  return useEvent(handler, [
+    'onGestureHandlerStateChange',
+    'onGestureHandlerEvent',
+  ]);
 }
