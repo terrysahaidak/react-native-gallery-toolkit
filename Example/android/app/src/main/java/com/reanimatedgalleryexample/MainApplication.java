87ab47e34621e1fdbe4a07d31c0e1648beaa5ed3
Terry Sahaidak <s_tasssik_taras@ukr.net>
2020-09-10 12:57:30 +0300

    Fix android build

--- Example/android/app/src/main/java/com/reanimatedgalleryexample/MainApplication.java
+++ Example/android/app/src/main/java/com/reanimatedgalleryexample/MainApplication.java
@@ -51,7 +51,7 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
+    // initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
 
   /**
@@ -61,27 +61,27 @@ public class MainApplication extends Application implements ReactApplication {
    * @param context
    * @param reactInstanceManager
    */
-  private static void initializeFlipper(
-      Context context, ReactInstanceManager reactInstanceManager) {
-    if (BuildConfig.DEBUG) {
-      try {
-        /*
-         We use reflection here to pick up the class that initializes Flipper,
-        since Flipper library is not available in release mode
-        */
-        Class<?> aClass = Class.forName("com.reanimatedgalleryexample.ReactNativeFlipper");
-        aClass
-            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-            .invoke(null, context, reactInstanceManager);
-      } catch (ClassNotFoundException e) {
-        e.printStackTrace();
-      } catch (NoSuchMethodException e) {
-        e.printStackTrace();
-      } catch (IllegalAccessException e) {
-        e.printStackTrace();
-      } catch (InvocationTargetException e) {
-        e.printStackTrace();
-      }
-    }
-  }
+  // private static void initializeFlipper(
+  //     Context context, ReactInstanceManager reactInstanceManager) {
+  //   if (BuildConfig.DEBUG) {
+  //     try {
+  //       /*
+  //        We use reflection here to pick up the class that initializes Flipper,
+  //       since Flipper library is not available in release mode
+  //       */
+  //       Class<?> aClass = Class.forName("com.reanimatedgalleryexample.ReactNativeFlipper");
+  //       aClass
+  //           .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
+  //           .invoke(null, context, reactInstanceManager);
+  //     } catch (ClassNotFoundException e) {
+  //       e.printStackTrace();
+  //     } catch (NoSuchMethodException e) {
+  //       e.printStackTrace();
+  //     } catch (IllegalAccessException e) {
+  //       e.printStackTrace();
+  //     } catch (InvocationTargetException e) {
+  //       e.printStackTrace();
+  //     }
+  //   }
+  // }
 }
