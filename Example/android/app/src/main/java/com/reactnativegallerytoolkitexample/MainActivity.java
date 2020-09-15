32c142c10dd142d20af374b4f7f1e077155748fc
Terry Sahaidak <s_tasssik_taras@ukr.net>
2020-09-15 14:52:39 +0300

    Fix namnig

--- Example/android/app/src/main/java/com/reactnativegallerytoolkitexample/MainActivity.java
+++ Example/android/app/src/main/java/com/reactnativegallerytoolkitexample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactNativegallerytoolkitexample;
+package com.reactnativegallerytoolkitexample;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
